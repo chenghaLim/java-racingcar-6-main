3af49d7816cb87b7773e49f57255ff1f9f49be6b
cheonghaLim <thddns12369@naver.com>
2024-06-07 16:44:47 +0900

    Delete Desktop directory

--- Desktop/NC13board/src/main/java/BoardMain.java
+++
@@ -1,9 +0,0 @@
-import Viewer.BoardViewer;
-import Viewer.UserViewer;
-
-public class BoardMain {
-    public static void main(String[] args){
-        UserViewer userViewer = new UserViewer();
-        userViewer.indexShow();
-    }
-}
